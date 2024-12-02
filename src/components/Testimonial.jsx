8980ac7f8a8fcf3c71ef9f70f0cf905300042aa3
Oni Abdullahi Oluwabusayo <busayaooni@gmail.com>
2024-12-02 13:34:57 +0100

    v2

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -6,7 +6,7 @@ import Image from '../assets/16Image.png'
 const Testimonial = () => {
   return (
     <>
-    <section className='font-Matter lg:p-[50px_100px] p-4 text-center '>
+    <section className='font-Matter  lg:p-[50px] p-4 text-center '>
     <p className='font-[400] text-[18px] text-[#684B06] '>Testimonial</p>
     <h1 className='font-[600] lg:text-[44px] text-[24px] text-[#101010] '>Hear From Our Esteemed Customers</h1>
     <section >
@@ -15,7 +15,7 @@ const Testimonial = () => {
             <TestSamp />
 
             </div>
-            <div>
+            <div className='lg:w-1/2'>
                 <img src={Image} alt="" />
                 <button className=' text-[18px] w-1/2 lg:w-auto bg-[#501720] text-white p-[8px_20px] mt-10 font-[600] rounded-full cursor-pointer ' ><a href="/">WATCH MORE</a></button>
 
--- src/components/productcomponents/productAll.jsx
+++ src/components/productcomponents/productAll.jsx
@@ -4,11 +4,13 @@ import Benefits from './Benefits';
 import Features from './Features';
 import TakefulInsurance from '../AboutPage/TakefulInsurance'
 import Footer from '../Footer'
+import Nav from '../nav'
 
 
 const productAll = () => {
   return (
     <div>
+      <Nav />
         <Hero />
       <Benefits />
       <TakefulInsurance />
