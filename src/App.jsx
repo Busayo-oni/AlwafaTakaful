65eeb4ce4d9ca1bd663b837be3f4a436648a2ebf
Oni Abdullahi Oluwabusayo <busayaooni@gmail.com>
2024-12-03 10:26:46 +0100

    v4

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import  BoardOfDirectors  from './components/AboutPage/BoardOfDirectors'
 import  OurStory  from './components/AboutPage/OurStory'
 import  AdvisoryTeam  from './components/AboutPage/AdvisoryTeam'
 import  ManagementStaff  from './components/AboutPage/ManagementStaff'
-import  Header  from './components/AboutPage/Header'
 import ProductAll from './components/productcomponents/productAll'
 import Education from './components/Education/productAll'
 import Credit from './components/credit/productAll'
@@ -19,15 +18,12 @@ import ClaimSection from './components/Claim/claimSection'
 import ContactSection from './components/Contact/ContactSection'
 import Calculator from './components/Calculator/CalculatorSection'
 import Generate from './components/Calculator/Generate'
-<<<<<<< HEAD
 import Faq from './components/Faq';
 
-=======
 import FidelityAll from './components/FidelityComponents/FidelityAll';
 import TravelAll from './components/TravelComponents/TravelAll';
 import MoneyAll from './components/MoneyComponents/MoneyAll';
 import MarineAll from './components/Marinecomponents/MarineAll'
->>>>>>> c38ff3bc334d121db743fd3959a2d394c4a1158a
 
 import HomeAll from './components/HomeAll';
 function App() {
@@ -57,7 +53,6 @@ function App() {
         <Route path="ClaimSection" element={<ClaimSection />} />
         <Route path="ClaimSuccess" element={<ClaimSuccess />} />
         <Route path="Faq" element={<Faq />} />
-
         <Route path="MarineAll" element={<MarineAll />} />
         <Route path="FidelityAll" element={<FidelityAll />} />
         <Route path="TravelAll" element={<TravelAll />} />
